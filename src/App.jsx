67e8891be10bfe8b2b1334bccb61bc3f302f7f69
diyora <dimekh@mail.ru>
2023-06-26 09:06:30 +0500

    feat: add logs to debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { Outlet, useMatch } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 import { Main } from "./components/Pages/Main";
 import { getProfile } from "./services/getProfile";
-import { useDispatch } from "react-redux";
 import useTelegram from "./hooks/useTelegram";
 import { userDataActions } from "./store/slices/userData";
 
 function App() {
   const userInitialData = useTelegram();
+  const userTelegramData = useSelector((state) => state.userTelegramData?.data);
   const dispatch = useDispatch();
   const match = useMatch("/");
 
-  console.log("initial data", userInitialData);
+  console.log("tg data", userTelegramData);
 
   if (userInitialData?.guid) {
     getProfile(userInitialData?.guid).then((res) => {
