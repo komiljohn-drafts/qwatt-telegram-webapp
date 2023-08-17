c71bf2f7dee1fb15c8198abb52c105dcc6a322e5
Nurdaulet Shinpolatov <nshinpolatov@gmail.com>
2023-08-17 14:21:09 +0500

    added faq page

--- src/components/Pages/Faq/index.jsx
+++ src/components/Pages/Faq/index.jsx
@@ -13,44 +13,29 @@ const FaqPage = () => {
   const userTelegramData = useSelector((state) => state.userTelegramData?.data);
   const [openCard, setOpenCard] = useState(null);
 
-  const sendMsg = (msg) => {
-    fetch(`https://api.telegram.org/bot5933951945:AAGVK6UU0GhoLrnGDPzQ22V681pYr4j-N5E/sendMessage`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          chat_id: "1780780393",
-          text: msg,
-        }),
-      })
-        .then(response => response.json())
-        .then(data => {
-          console.log("Message sent successfully", data);
-        })
-        .catch(error => {
-          console.error("Error sending message", error);
-        });
+  const langObj = {
+    en: "english",
+    ru: "russian",
+    uz: "uzbek"
   }
+
+  const lang = langObj[userTelegramData?.language_code]
   
   const { t } = useTranslation();
 
   const getFaq = () => {
-    sendMsg(userTelegramData?.language_code)
     getFaqs({
       data: {
-        language: userTelegramData?.language_code,
-        // language: 'russian',
+        language: lang,
         type: "Faq"
       }
     })
       .then((res) => {
         if(res?.data?.data?.data?.response){
-          console.log(res?.data?.data?.data?.response);
-          setData(res?.data?.data?.data?.response);
+          setData(res?.data?.data?.data?.response || []);
         } else if(res?.data?.data?.data?.error) {
           setErrorAlertOpen(true);
-        }
+        } 
       })
       .catch(() => {
         setErrorAlertOpen(true);
@@ -61,7 +46,7 @@ const FaqPage = () => {
     getFaq();
   }, []);
 
-  if (!data) return <FullScreenSpinner />
+  if (!data?.length) return <FullScreenSpinner />
   return (
     <div className={styles.faqWrapper}>
       {data?.map((item, index) => (
