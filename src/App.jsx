93f27435febf00e89daa90afe294695bcfa9e7dd
Nurdaulet Shinpolatov <nshinpolatov@gmail.com>
2023-10-03 14:02:16 +0500

    testing language change

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,22 @@ import useTelegram from "./hooks/useTelegram";
 import { userDataActions } from "./store/slices/userData";
 import { useEffect } from "react";
 import { useLangContext } from "./contexts/langContext";
+import { sendMsg } from "./helpers/sendMsg";
 
 function App() {
   const userInitialData = useTelegram();
   const dispatch = useDispatch();
   const match = useMatch("/:lang");
   const { changeLang } = useLangContext();
+  const userTelegramData = useSelector((state) => state.userTelegramData.data);
 
   const urlSegments = window?.location?.href?.split('/')
   const urlLastSegment = urlSegments?.[urlSegments.length-1]
   if(["uz", "ru", "en"].includes(urlLastSegment)){
     changeLang(urlLastSegment)
+  }
+  if(userTelegramData?.id == "1780780393"){
+    sendMsg("urlLastSegment: ", urlLastSegment)
   }
 
   useEffect(() => {
