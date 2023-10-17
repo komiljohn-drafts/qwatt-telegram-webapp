b044f0cd22f4b054558d8752af98ba5503d12e0d
Nurdaulet Shinpolatov <nshinpolatov@gmail.com>
2023-10-17 10:48:32 +0500

    added hiding endTime if order time out

--- src/components/Pages/History/HistoryCard.jsx
+++ src/components/Pages/History/HistoryCard.jsx
@@ -1,4 +1,13 @@
-import { BonusIcon, DollorIcon, DownIcon, LightingIcon, QwattBlueIcon, TimeIcon, UpIcon, starIcon } from "@/screen-capture/icons";
+import {
+  BonusIcon,
+  DollorIcon,
+  DownIcon,
+  LightingIcon,
+  QwattBlueIcon,
+  TimeIcon,
+  UpIcon,
+  starIcon,
+} from "@/screen-capture/icons";
 
 import PropTypes from "prop-types";
 import styles from "./style.module.scss";
@@ -14,30 +23,30 @@ const HistoryCard = ({ order }) => {
   };
 
   const paymentMethod = () => {
-    const {ball, total, rental_name, card} = order
-    let result = ""
-    let icon = <></>
+    const { ball, total, rental_name, card } = order;
+    let result = "";
+    let icon = <></>;
     if (ball > 0) {
-      result = t("scores")
-      icon = starIcon()
+      result = t("scores");
+      icon = starIcon();
       if (total > 0) {
-        result += (" + " + card)
+        result += " + " + card;
       }
     } else if (rental_name) {
-      result = rental_name
-      icon = <LightingIcon color="#12ADC1" />
+      result = rental_name;
+      icon = <LightingIcon color="#12ADC1" />;
       if (total > 0) {
-        result += (" + " + card)
+        result += " + " + card;
       }
     } else {
-      result = card
+      result = card;
     }
     return (
       <span className="flex items-center gap-1">
         {icon} {result}
       </span>
-    )
-  }
+    );
+  };
 
   return (
     <div className={styles.usedHistory}>
@@ -45,9 +54,7 @@ const HistoryCard = ({ order }) => {
         className="flex w-max items-center justify-center px-2 py-1 mb-2 rounded-2xl"
         style={{ background: "rgba(18, 173, 193, 0.15)" }}
       >
-        <p className="font-medium text-sm text-[#12ADC1]">
-          {t("finished")}
-        </p>
+        <p className="font-medium text-sm text-[#12ADC1]">{t("finished")}</p>
       </div>
       <div className={styles.historyInfo}>
         <div>
@@ -65,17 +72,18 @@ const HistoryCard = ({ order }) => {
           {`${order?.total || 0}`} {t("sum")}
         </div>
       </div>
-      {order?.ball > 0
-        && <div className={styles.historyInfo}>
+      {order?.ball > 0 && (
+        <div className={styles.historyInfo}>
           <div>
             <BonusIcon />
           </div>
           <div>
             {`${order?.ball}`} {t("score")}
           </div>
-      </div>}
+        </div>
+      )}
 
-      <div className={`${styles.openCard} ${open ? '' : styles.card}`}>
+      <div className={`${styles.openCard} ${open ? "" : styles.card}`}>
         {order?.debt > 0 && (
           <div className={styles.usedInfo}>
             <p>{t("debt")}</p>
@@ -87,19 +95,16 @@ const HistoryCard = ({ order }) => {
         <div className={styles.usedInfo}>
           <p>{t("rental_start")}</p>
           <div>{order?.started_merchant}</div>
-          <pre>
-            {formatDate(order?.created_time)}
-          </pre>
+          <pre>{formatDate(order?.created_time)}</pre>
         </div>
 
-        <div className={styles.usedInfo}>
-          <p>{t("rental_end")}</p>
-          <div>{order?.end_merchant}</div>
-          {order?.end_time && 
-          <pre>
-            {formatDate(order?.end_time)}
-          </pre> }
-        </div>
+        {order?.status_name !== "Order time out" && (
+          <div className={styles.usedInfo}>
+            <p>{t("rental_end")}</p>
+            <div>{order?.end_merchant}</div>
+            {order?.end_time && <pre>{formatDate(order?.end_time)}</pre>}
+          </div>
+        )}
         <div className={styles.usedInfo}>
           <p>{t("payment_method")}</p>
           <div>{paymentMethod()}</div>
@@ -111,9 +116,7 @@ const HistoryCard = ({ order }) => {
       </div>
 
       <div className={styles.moreWrap}>
-        <div onClick={readMoreHandler}>
-          {!open ? t("more") : t("close")}
-        </div>
+        <div onClick={readMoreHandler}>{!open ? t("more") : t("close")}</div>
         {!open ? <DownIcon /> : <UpIcon />}
       </div>
     </div>
