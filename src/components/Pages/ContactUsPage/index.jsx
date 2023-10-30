8f74d16a5a75b5b34f0c8e3e4e9762eb9550ef22
Nurdaulet Shinpolatov <nshinpolatov@gmail.com>
2023-10-30 10:45:07 +0500

    added telegram sup username to contact us

--- src/components/Pages/ContactUsPage/index.jsx
+++ src/components/Pages/ContactUsPage/index.jsx
@@ -7,8 +7,13 @@ const ContactUsPage = () => {
     <div className={styles.contactUsWrapper}>
       <div className={styles.container}>
         <p className={styles.label}>{ t("our_phone_number") }</p>
-        <button className={styles.circle} onClick={() => window.open('tel:+998954748080', '_blank')}>
+        <button onClick={() => window.open('tel:+998954748080', '_blank')}>
           +998 95 474 80 80
+        </button>
+
+        <p className={styles.label}>Telegram</p>
+        <button onClick={() => window.open('https://t.me/qwattsupbot', '_blank')}>
+          @qwattsupbot
         </button>
         
         <p className={styles.label}>{ t("website") }</p>
