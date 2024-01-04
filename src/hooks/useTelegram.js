9e7ffd1ffe002b56c2807f6240007b3c407b5896
komiljon_yormurodov <komiljonyormurodov@gmail.com>
2024-01-04 15:29:56 +0500

    initial commit

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -25,11 +25,11 @@ const useTelegram = () => {
     // setTgID("6225306070");
     // setTgID("6267637476");
 
-    // setTgID("1413774013") // 17 91
+    // setTgID("1413774013"); // 17 91
     // setTgID("1780780393") // 50 01
     // setTgID("6508689707") // Damir test
     // setTgID("579391823") // Damir 2272
-    // setTgID("754650976") // Mukarramhon PM
+    setTgID("754650976"); // Mukarramhon PM
     // setTgID("857348252") // Mukarramhon PM 2
     // setTgID("5945885433") // Sherzod mobile
     // setTgID("371594426") // One of clients
